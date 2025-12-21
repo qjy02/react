6ab1407227c93002720ce4b51cac34aadbf6a305
qjy02 <jyquek32@gmail.com>
2025-12-21 17:51:00 +0800

    Update: Fix error

--- src/header.jsx
+++ src/header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <header className="center header-theme p-4">
         <a href="/">
             <img 
-                src='/Vispiv.png' 
+                src='Vispiv.png'
                 alt='Vispiv Logo' 
                 className='h-12 sm:h-16 mr-4 cursor-pointer' 
             />
